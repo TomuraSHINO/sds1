0e2f061ba4ae9b4a0ee7cfe477c642410d247a86
Daniel Rocha <TomuraSHINO@protonmail.com>
2020-09-20 13:23:00 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-daniel.herokuapp.com/games';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<barChartData[]>([]);
